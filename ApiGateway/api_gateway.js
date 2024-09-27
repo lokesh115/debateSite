1f84629c711023429144838d5bbb2719d61d3c55
Avinash Nowpada <lokeshnowpada@gmail.com>
2024-09-27 16:43:56 +0530

    configured docker

--- ApiGateway/api_gateway.js
+++ ApiGateway/api_gateway.js
@@ -2,11 +2,11 @@ var proxy = require('express-http-proxy');
 var app = require('express')();
 
 //Frontend
-const uiServiceURI = "localhost:1090"
+const uiServiceURI = "http://frontend:1090"
 app.use('/uiService', proxy(uiServiceURI))
 
 //userService
-const userServiceURI = "localhost:8081"
+const userServiceURI = "http://userservice:8081"
 app.use('/userService', proxy(userServiceURI))
 
 const PORT = 7878
--- UserService/config/database_config.js
+++ UserService/config/database_config.js
@@ -1,14 +1,19 @@
-function config(){
+function config() {
     const mongoose = require('mongoose');
-    // MongoDB connection URL
-    const mongoURI = 'mongodb://localhost/UserService';
- 
+
+    // MongoDB connection URL - use the container name (mongodb) instead of localhost
+    const mongoHost = 'mongodb';
+    const mongoPort = '27017';
+    const mongoDB = 'UserService';
+    
+    const mongoURI = `mongodb://${mongoHost}:${mongoPort}/${mongoDB}`;
+
     // Connection options
     const options = {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     };
- 
+
     // Establish the connection
     mongoose.connect(mongoURI, options)
         .then(() => {
@@ -16,33 +21,21 @@ function config(){
         })
         .catch((error) => {
             console.error('Error connecting to MongoDB:', error.message);
- 
-            // Handle specific error conditions
-            if (error.name === 'MongoNetworkError') {
-                console.error('Network error occurred. Check your MongoDB server.');
-            } else if (error.name === 'MongooseServerSelectionError') {
-                console.error('Server selection error. Ensure'
-                    + ' MongoDB is running and accessible.');
-            } else {
-                // Handle other types of errors
-                console.error('An unexpected error occurred:', error);
-            }
         });
- 
-    // Handling connection events
+
     const db = mongoose.connection;
- 
+
     db.on('error', (error) => {
         console.error('MongoDB connection error:', error);
     });
- 
+
     db.once('open', () => {
         console.log('Connected to MongoDB');
     });
- 
+
     db.on('disconnected', () => {
         console.log('Disconnected from MongoDB');
     });
-    }
-    
-module.exports = config
+}
+
+module.exports = config;
